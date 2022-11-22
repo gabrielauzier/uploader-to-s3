04b920e5ae3b9effcde5c06d70faca01df5a4246
gabvask <gabriel@loldesign.com.br>
2022-11-22 15:16:46 -0400

    refactor: delete unused setup

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,6 @@ import path from "path";
 
 const app = express();
 
-/** Database setup */
-// const mongoose = require("mongoose");
-// mongoose.connect("mongodb://localhost:27017/upload");
-
 app.use(express.json()); //para lidar com o corpo de mensagens tipo json
 app.use(express.urlencoded({ extended: true })); // como deve lidar para requisições no padrão especifico
 app.use(morgan("dev"));
